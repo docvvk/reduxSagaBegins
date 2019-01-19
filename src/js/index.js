873ac9d21185453dd5b43c63b1ccba9bb100a95d
Vivek Malhan <vivek.malhan@yahoo.com>
2019-01-19 15:22:58 +0530

    adding components

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import store form "./store/index";
+import store from "./store/index";
 import App from "./components/App.jsx";
 
 render (
